92b3381b915f8323c0a94e95b1bd7a8d5b2d408e
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-01 17:55:20 +0300

    create index function

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import datetime as dt
+from django.contrib.auth.models import User
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile
 
 # Create your views here.
+def index(request):
+    posts = Image.objects.all()
+    all_users = User.objects.exclude(id=request.user.id)
+    current_user = request.user
+
+    return render (request, 'all-insta/home.html',{"posts":posts,"all-users":all_users,"current_user":current_user})
