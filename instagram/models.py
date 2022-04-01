49e74a95fdb37b61dab394e9da98eda3ed9fdc92
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-01 21:56:56 +0300

    add image property

--- instagram/models.py
+++ instagram/models.py
@@ -1,16 +1,15 @@
-from email.policy import default
-from unicodedata import name
+
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
 # Create your models here.
 
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length=100,blank=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='images')
     pub_date = models.DateField(auto_now_add=True)
     caption = models.CharField(max_length=200,blank=True)
+    image = models.ImageField(upload_to = 'images/')
 
 
 
