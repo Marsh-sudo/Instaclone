d5d714b72f381425bb2d2bab742f1e15807dc0b3
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-03 21:37:24 +0300

    create tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Image,Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ImageTestCase(TestCase):
+    def setUp(self):
+
+        self.user = User.objects("biggie")
+
+        self.my_profile = Profile(profile_pic='image.png',bio="my bio",user=self.user)
+        self.my_profile.save()
+
+        self.photo = Image(pic='image.png', caption="posting", profile=self.my_profile)
+
+    def test_instance_true(self):
+        self.photo.save()
+        self.assertTrue(isinstance(self.photo, Image))
+
+    def test_save_image(self):
+        self.photo.save()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 1)
+
+
