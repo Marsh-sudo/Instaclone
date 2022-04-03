5cadef7ac3846539523162ac42f69a45d1963b95
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-04-03 21:38:01 +0300

    create path for like

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import re_path
 from . import views
 from django.conf import settings
@@ -5,6 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     re_path('^$',views.index,name="home"),
+    re_path('^new/image$',views.new_post,name = 'new-post'),
+    re_path('search/',views.search_results,name='search_results'),
+    re_path('like/<id>',views.like,name='like'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
